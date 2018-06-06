9270297cebf0cb7880c33f2feb46b938e37e73c2
tsunecko <malkavian_child@bk.ru>
2018-06-06 11:59:11 +0300

    create calendar

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    "use strict";
+
+    let headerDate = () => {
+        let thisDate = new Date();
+        
+
+        //TODO получить дату типа ВТОРНИК, 5 ИЮНЯ 2018
+    }
+
+  });
