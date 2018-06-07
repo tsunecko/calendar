49bec62a32da17f4f01e9b9dece8973213232fdb
tsunecko <malkavian_child@bk.ru>
2018-06-07 10:12:54 +0300

    generate full week with previous and next month`s days

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     "use strict";
 
+    let thisDate = new Date();
+    let arrayWeek = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+    let arrayMonth = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    let arrayMonthDays = [31, [28, 29], 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let thisDay = thisDate.getDate();
+    let thisYear = thisDate.getFullYear();
+    let thisMonth = thisDate.getMonth();
+
     /******************* create header date **********************/
 
     let headerDate = () => {
-        let thisDate = new Date();
-
-        let arrayWeek = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
-        let thisWeekDay = arrayWeek.find((day, index) => {
+        let thisWeekDayName = arrayWeek.find((day, index) => {
             return index === thisDate.getDay();
-        })
-
-        let thisDay = thisDate.getDate();
-
-        let arrayMonth = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+        });
         let thisMonthName = arrayMonth.find((month, index) => {
             return index === thisDate.getMonth();
-        })
-
-        let thisYear = thisDate.getFullYear();
-
-        return `${thisWeekDay}, ${thisDay} ${thisMonthName} ${thisYear}`;
+        });
+        return `${thisWeekDayName}, ${thisDay} ${thisMonthName} ${thisYear}`;
     }
 
     document.getElementById('header-text').innerHTML=headerDate();
 
-
     /******************* create weekdays **********************/
 
     (function weekDays() {
-        let arrayWeek = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
         let weekDays = document.getElementById("week-days");
-
         for (let i = 0; i < arrayWeek.length; i++) {
             let div = document.createElement('span');
             div.className = "calendar__body_weekday_cell";
@@ -40,17 +35,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     })();
 
-    
     /******************* create month days **********************/
 
     (function monthDays() {
-        let thisDate = new Date();
-        let arrayMonthDays = [31, [28, 29], 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        let thisMonth = thisDate.getMonth();
-        let thisYear = thisDate.getFullYear();
-        let countDays = [];
 
         /*********** generate array of days ***********/
+
+        /* generate days of previous month */
+        let prevCountDays = [];
+        let firstDay = new Date(thisYear, thisMonth, 1);
+        let prewDays = firstDay.getDay() - 1;
+        if (prewDays == -1) prewDays = 6;
+        let prewMonth = arrayMonthDays[thisMonth - 1];
+        for(let i = prewMonth - prewDays; i != prewMonth; i++) {
+            prevCountDays.push(i);
+        }
+
+        /* generate days of this month */
+        let countDays = [];
         if (thisMonth === 1) {
             if (thisYear % 4 === 0) {
                 for (let i = 1; i <= arrayMonthDays[1][1]; i++) {
@@ -67,6 +69,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
             }
         }
 
+        /* generate days of next month */
+        let nextCountDays = [];
+        let lastDay = new Date(thisYear, thisMonth, countDays.length);
+        let nextDays = 7 - lastDay.getDay();
+        if (nextDays == 7) nextDays = 0;
+        let nextMonth = arrayMonthDays[thisMonth + 1];
+        for(let i = nextMonth - nextDays; i != nextMonth; i++) {
+            nextCountDays.push(i);
+        }
+
         /*********** insert days in calendar body ***********/
         
         let weekDays = document.getElementById("calendar-body");
@@ -74,12 +86,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
         div.className = "calendar__body_weekday";
         weekDays.appendChild(div);
 
-        for (let i = 0; i < countDays.length; i++) {
+        generateDays(prevCountDays, "another", div);
+        generateDays(countDays, "calendar__body_weekday_cell", div);
+        generateDays(nextCountDays, "another", div);
+    })();
+
+    /******************* help function for insert days **********************/
+    
+    function generateDays(array, className, div) {
+        for (let i = 0; i < array.length; i++) {
             let span = document.createElement('span');
-            span.className = "calendar__body_weekday_cell";
-            span.innerHTML = countDays[i];
+            span.className = className;
+            span.innerHTML = array[i];
             div.appendChild(span);
         }
-    })();
+    }
     
   });
